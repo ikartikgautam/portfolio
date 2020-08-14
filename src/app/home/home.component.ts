25c67e78fbf90b1ede4d86a78954557f2b8af963
thiskartik <gautam.kartik05@gmail.com>
2020-08-14 18:14:57 +0530

    added technologies section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,6 +19,22 @@ export class HomeComponent implements OnInit {
     { skill: 'JAVA', pro: '65%' },
     { skill: 'JAVASCRIPT', pro: '90%' },
     { skill: 'TYPESCRIPT', pro: '60%' },
+    { skill: 'PHP', pro: '50%' },
+    { skill: 'DART', pro: '75%' },
   ];
+
+  techs = [
+    { skill: 'ANGULAR', pro: '90%' },
+    { skill: 'EXPRESS APP', pro: '80%' },
+    { skill: 'NODE JS', pro: '70%' },
+    { skill: 'SQL DB', pro: '95%' },
+    { skill: 'FLUTTER', pro: '70%' },
+    { skill: 'AWS/CLOUD', pro: '65%' },
+    { skill: 'WEB SCRAPING', pro: '95%' },
+    { skill: 'DATA ANALYSIS', pro: '50%' },
+    { skill: 'ML', pro: '45%' },
+    { skill: 'IOT', pro: '35%' },
+    { skill: 'LINUX', pro: '20%' },
+  ]
 
 }
